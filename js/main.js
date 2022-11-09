294e339aaacc83e3f7240b2ad88346c7284cf17a
Jessica <jesscara.993@gmail.com>
2022-11-09 14:50:39 +0100

    created variable for div inside container

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 // VARIABILI
-// const ul = document.querySelector("ul.list");
+const contenitore = document.querySelector(".container");
 
 // STAMPARE NUMERI DA 1 A 100
 for (let i = 1; i <= 100; i++) {
-  
+
   //MULTIPLI DI 3 E 5
   if (i % 3 === 0 && i % 5 === 0){
     console.log("FizzBuzz");
@@ -21,4 +21,8 @@ for (let i = 1; i <= 100; i++) {
   else{
     console.log(i);
   }
+
+  const element = document.createElement("div");
+  element.append(i);
+  contenitore.append(element);
 }
