8496a742802fa031056b050d4449d6622c4d241d
Jessica <jesscara.993@gmail.com>
2022-11-09 14:08:15 +0100

    js comments before ex

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// VARIABILI
+
+// STAMPARE NUMERI DA 1 A 100
+
+// MULTIPLI DI 3
+
+//MULTIPLI DI 5
+
+//MULTIPLI DI 3 E 5
